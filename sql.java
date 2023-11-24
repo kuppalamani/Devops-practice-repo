d3b1b520c8f0cb3789ffdeb892717b2273acdef6
Mani <root@ip-172-31-30-251.ec2.internal>
2023-11-24 07:31:06 +0000

    SQL & Testing commit

---
+++ sql.java
@@ -0,0 +1,8 @@
+SQL:
+ 
+
+➢	Creating a table into the database using DDL commands.
+➢	Insert the new data, update existing data, delete existing data from database using DDL commands.
+➢	Arranging the data using order by command.
+➢	Manipulate the information using Functions like Single row functions and Multi row functions.
+
---
+++ testing.java
@@ -0,0 +1,9 @@
+Manual Testing:
+•	Having good knowledge in software development life cycle-SDLC.
+•	Smoke testing on basic, critical features of application
+•	Process of Regression testing after bug fixes.
+•	Complete Knowledge on Defect life cycle and its various statuses.
+•	Importance of test scenarios and test cases.
+•	Good knowledge on TYPES of testing.
+•	Knowledge on process of identifying impact area..
+
