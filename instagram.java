eea1dfe3762f4e2510d1e2c8f53791d16189cbf1
root <root@ip-172-31-30-251.ec2.internal>
2023-11-24 07:02:27 +0000

    first commit

---
+++ instagram.java
@@ -0,0 +1,14 @@
+Core JAVA:
+•	Features of JAVA.
+•	Knowledge in Installation of Java.
+•	Tokens, Basic programming, Control statements having knowledge .
+•	Creation of Class objects and having knowledge of Constructors and Blocks.
+•	Good knowledge in OOPs concepts includes Inheritance, Polymorphism, Encapsulation and Abstraction.
+SQL:
+ 
+
+➢	Creating a table into the database using DDL commands.
+➢	Insert the new data, update existing data, delete existing data from database using DDL commands.
+➢	Arranging the data using order by command.
+➢	Manipulate the information using Functions like Single row functions and Multi row functions.
+
